ceb2c8ede788578aa83fd23e499f0ac2ea8c2d25
elPangolin <nathan.laubeuf@gmail.com>
2017-04-21 17:30:29 +0200

    commenting

--- acquisition/file_writer.py
+++ acquisition/file_writer.py
@@ -4,8 +4,10 @@ import csv
 from PyQt5.QtCore import (pyqtSlot, QObject, QThread, pyqtSignal)
 
 
-class FileWritter(QObject):
-
+class FileWriter(QObject):
+    """
+    Class to save acquired data
+    """
     makeFileNameSig = pyqtSignal()
     patientNum = 0
     sessionNum = 0
@@ -21,6 +23,10 @@ class FileWritter(QObject):
 
     @pyqtSlot()
     def makeFileName(self):
+        """
+        Build the path of the file where the data will be saved
+        format : working_dir/patientNum_sessionNum_testNum.csv
+        """
         try:
             self.fileName = self.workDir + '/' + str(self.patientNum) + '_' + str(self.sessionNum) + '_' + str(self.testNum) + ".csv"
         except TypeError:
@@ -30,29 +36,37 @@ class FileWritter(QObject):
 
     @pyqtSlot(int)
     def setPatientNum(self, num):
+        """Called when the patientNum is changed"""
         self.patientNum = num
         print("Patient num = ", str(self.patientNum))
         self.makeFileNameSig.emit()
 
     @pyqtSlot(int)
     def setSessionNum(self, num):
+        """Called when the sessionNum is changed"""
         self.sessionNum = num
         print("Session num = ", str(self.sessionNum))
         self.makeFileNameSig.emit()
 
     @pyqtSlot(int)
     def setTestNum(self, num):
+        """Called when the TestNum is changed"""
         self.testNum = num
         print("Test num = ", str(self.testNum))
         self.makeFileNameSig.emit()
 
     @pyqtSlot(str)
     def setWorkDir(self, dir_name):
+        """Called when the workingDir is changed"""
         self.workDir = dir_name
         print("Dir Name :" + self.workDir)
         self.makeFileNameSig.emit()
 
     def startRecord(self):
+        """
+        Opens the file and assign a writer to it
+        raises errors if the specified directory doesn't exist or if the file already exists
+        """
         if self.workDir == None:
             raise NotADirectoryError
         elif os.path.isfile(self.fileName):
@@ -64,9 +78,11 @@ class FileWritter(QObject):
 
     @pyqtSlot(float)
     def writeValue(self, value):
+        """writes the received values into he specified file"""
         self.writer.writerow([value])
 
     @pyqtSlot()
     def stopRecord(self):
+        """closes the file when recording session is over"""
         self.file.close()
 
